f14f05ebce129bee9368f9f8f216ea04520f68e6
MoJo <msjche@gmail.com>
2020-05-01 18:52:24 -0700

    add the stuff

---
+++ dmenu/config.def.h
@@ -0,0 +1,39 @@
+/*       _ __ ___  ___ (_) ___| |__   ___               */
+/*      | '_ ` _ \/ __|| |/ __| '_ \ / _ \              */
+/*      | | | | | \__ \| | (__| | | |  __/              */
+/*      |_| |_| |_|___// |\___|_| |_|\___|              */
+/*                   |__/                               */
+/*             MoJo's dmenu config                      */
+
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 600;                    /* minimum width when centered */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"Dejavu San Mono for Powerline:size=10",
+	"JoyPixels:size=9:antialias=true:autohint=true",
+
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#4d63b3" },
+	[SchemeSelHighlight] = { "#ffc978", "#005577" },
+	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 20;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width = 5;
---
+++ dwmblocks/blocks.h
@@ -0,0 +1,44 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/	 	/*Update Interval*/	/*Update Signal*/
+    {"", "music",		    	2,		        	2},
+
+	{"", "cat /tmp/recordingicon 2>/dev/null",	2,	9},
+
+	{"", "weather",		    	300,	            2},
+
+	{" ", "torrents",			5,  				7},
+
+/*	{"", "upt",		        60,		            2}, */
+
+	{" 📦 ", "pacupdate",		300,		        8},
+
+/*	{"🐝 ", "bumblebee",		5,			        8}, */
+	
+/*	{"🗄 ", "disk /",			60,		            1}, */
+	
+/*	{"  ", "disk /home",       60,		            1}, */
+
+	{"🧠 ", "memory",	        6,		            1},
+
+	{"🖥 ", "cpu2",	        	2,		            1},
+
+	{"🌡", "cpu",	        	10,		            1},
+
+	{" 🌐 ", "bandwidth wlo1",	2,		            1},
+
+	{" ", "volume2",			2,		            10},
+
+	{" 🔆 ", "brightness",     	6,		            1},
+
+	{"", "battery",				5,  				12},
+
+	{"🗓 ", "clock",			10,		            0},
+
+	{"", "internet",			5,		            0},
+
+	{"", "vpn",					10,		            0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = ' ';
