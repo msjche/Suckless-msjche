d5464a3b94dd642f1301aa876c661f8b6b7e8876
MoJo <msjche@gmail.com>
2020-05-24 05:43:16 -0700

    update

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -33,7 +33,7 @@ static const char col_yellow[]          = "#b38c00"; /* border color */
  * 0xee adds wee bit of transparency.
  * Play with the value to get desired transparency.
  */
-static const unsigned int baralpha    = 0xff;
+static const unsigned int baralpha    = 0xee;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]        = {
     /*               fg         bg         border   */
